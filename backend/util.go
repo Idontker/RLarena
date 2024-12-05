5acd0961cf37b63e2d65bc7b481fcf6e8708e80a
yw97alos <karol.bakas@fau.de>
2024-12-06 00:42:19 +0100

    readded logging

--- backend/util.go
+++ backend/util.go
@@ -9,5 +9,5 @@ import (
 var logger = log.New(os.Stdout, "[http]: ", log.LstdFlags)
 
 func LogRequest(r *http.Request) {
-	// logger.Printf("Request: %s %s", r.Method, r.URL.String())
+	logger.Printf("Request: %s %s", r.Method, r.URL.String())
 }
